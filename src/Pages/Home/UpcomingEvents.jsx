70df3de685f9765a7ea30e0bdbea7a656ef628a9
shantunu007 <chakmashantunu.web@gmail.com>
2025-06-05 12:01:41 +0600

    upcoming events viewport motion done

--- src/Pages/Home/UpcomingEvents.jsx
+++ src/Pages/Home/UpcomingEvents.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import EventCard from '../Shared/EventCard';
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { div } from 'framer-motion/client';
 
 const UpcomingEvents = ({ events }) => {
@@ -9,22 +9,36 @@ const UpcomingEvents = ({ events }) => {
     const displayEvents = showAll ? events : events.slice(0, 8)
 
     return (
-        <div>
-            <h2 className="text-3xl">upcoming events of the day : {events.length}</h2>
-            <motion.div
-                initial={{ y: 50, opacity: 0 }}
-                whileInView={{ y: 0, opacity: 1 }}
-                transition={{ duration: 1, type: 'spring', stiffness: 80, damping: 10 }}
-                viewport={{ once: true, amount: 0.2 }}
-                className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4 my-24'>
-                {
-                    displayEvents.map(event => <EventCard key={event._id} event={event}></EventCard>)
-                }
-            </motion.div>
-            <div className='text-center my-8'>
-                <button onClick={() => setShowAll(!showAll)} className='btn btn-primary'>{showAll ? "Show Less" : "Show More"}</button>
+        <motion.div
+            initial={{ y: 50, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2, type: 'spring', stiffness: 80, damping: 10 }}
+            viewport={{ once: true, amount: 0.2 }}
+            className='my-24'>
+            <div className='text-center space-y-4'>
+                <h1 className='text-6xl'>Upcoming Events</h1>
+                <p>Explore today’s featured events and get involved in meaningful social development activities. Discover the events scheduled for today. Don’t miss out—stay updated and involved!</p>
             </div>
-        </div>
+            <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-4 my-24'>
+                <AnimatePresence>
+                    {
+                        displayEvents.map(event => (<motion.div key={event._id}
+                            layout
+                            initial={{ opacity: 0, y: 30 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 30 }}
+                            transition={{ duration: 0.3 }}
+                        >
+                            <EventCard event={event}></EventCard>
+                        </motion.div>))
+                    }
+                </AnimatePresence>
+            </div>
+
+            <div className='text-center my-8'>
+                <button onClick={() => setShowAll(prev => !prev)} className='btn btn-primary'>{showAll ? "Show Less" : "Show More"}</button>
+            </div>
+        </motion.div>
     );
 };
 
