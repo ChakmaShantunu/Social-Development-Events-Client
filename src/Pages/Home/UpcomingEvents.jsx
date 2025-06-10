e22265c09b8200fef3a76d42692831dd752c60f5
shantunu007 <chakmashantunu.web@gmail.com>
2025-06-10 20:03:05 +0600

    event type filter successful

--- src/Pages/Home/UpcomingEvents.jsx
+++ src/Pages/Home/UpcomingEvents.jsx
@@ -1,6 +1,5 @@
-import React, { use, useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import EventCard from '../Shared/EventCard';
-import { div } from 'framer-motion/client';
 import { AuthContext } from '../../contexts/AuthContexts/AuthContext';
 import Loading from '../../components/Loader/Loading';
 import { AnimatePresence, motion } from "motion/react"
@@ -9,27 +8,81 @@ const UpcomingEvents = () => {
 
     const { loading, user } = useContext(AuthContext);
 
-    const [events, setEvents] = useState([])
-    const today = new Date();
+    const [events, setEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("all");
+    const [searchTitle, setSearchTitle] = useState("");
+    const [inputText, setInputText] = useState('')
+    // const today = new Date();
 
-    const upcomingEvents = events.filter(event => new Date(event.eventDate) >= today);
+    // const upcomingEvents = events.filter(event => new Date(event.eventDate) >= today);
 
     const [showAll, setShowAll] = useState(false);
-    const displayEvents = showAll ? upcomingEvents : upcomingEvents.slice(0, 8)
+
+    // use
+
+
+
+
+    // useEffect(() => {
+
+    //     fetch(`${import.meta.env.VITE_API_URL}/events`, {
+    //         credentials: 'include'
+    //     })
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setEvents(data);
+    //             setFilteredEvents(data);
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         })
+    // }, [])
+
+
+
+    // useEffect(() => {
+    //     const today = new Date();
+
+    //     let upcoming = events.filter(event => new Date(event.eventDate) >= today);
+
+    //     if (selectedCategory !== 'all') {
+    //         upcoming = upcoming.filter(event => event.eventType === selectedCategory);
+    //     }
+
+    //     if (searchTitle.trim()) {
+    //         upcoming = upcoming.filter(e => e.title.toLowerCase().includes(searchTitle.toLowerCase()));
+    //     }
+
+    //     setFilteredEvents(upcoming);
+    // }, [events, selectedCategory, searchTitle])
+
+    const displayEvents = showAll ? events : events.slice(0, 8);
 
     useEffect(() => {
+        let query = '';
 
-        fetch(`${import.meta.env.VITE_API_URL}/events`, {
-            credentials: 'include'
-        })
+        if (selectedCategory !== 'all') {
+            query += `eventType=${selectedCategory}`
+        }
+
+        if (searchTitle.trim()) {
+            if (query) query += '&';
+            query += `title=${encodeURIComponent(searchTitle.trim())}`;
+        }
+
+        fetch(`${import.meta.env.VITE_API_URL}/events?${query}`)
             .then(res => res.json())
             .then(data => {
+                console.log(data);
                 setEvents(data);
             })
             .catch(error => {
                 console.log(error);
             })
-    }, [])
+
+    }, [selectedCategory, searchTitle])
+
 
     if (loading) {
         return <Loading></Loading>
@@ -46,7 +99,41 @@ const UpcomingEvents = () => {
                 <h1 className='text-6xl'>Upcoming Events</h1>
                 <p>Explore today’s featured events and get involved in meaningful social development activities. Discover the events scheduled for today. Don’t miss out—stay updated and involved!</p>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-x-4 gap-y-8 my-24 mx-4'>
+            <div className='flex justify-around items-center mt-12'>
+                <div className='flex justify-center items-center'>
+                    <label className="input w-2xl mr-4 h-12">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input type="search" value={inputText} onChange={e => setInputText(e.target.value)} className="grow" placeholder="Search" />
+                        <kbd className="kbd kbd-sm">⌘</kbd>
+                        <kbd className="kbd kbd-sm">K</kbd>
+                    </label>
+                    <button onClick={() => setSearchTitle(inputText)} className="btn">Search</button>
+                </div>
+            </div>
+
+            <div className='mt-12'>
+                <label className="label font-semibold mr-4">Category: </label>
+                <select name="eventType" value={selectedCategory} onChange={e => setSelectedCategory(e.target.value)} id="" className='select border' required>
+                    <option value="" disabled>Choose a category</option>
+                    <option value="all">All Event</option>
+                    <option value="Cleanup">Clean-up</option>
+                    <option value="Plantation">Tree Plantation</option>
+                    <option value="Donation">Blood Donation</option>
+                    <option value="Health">Health Camp</option>
+                </select>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-x-4 gap-y-8 mb-24 mt-12 mx-4'>
                 <AnimatePresence>
                     {
                         displayEvents.map(event => (<motion.div key={event._id}
